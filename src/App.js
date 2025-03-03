f4f5d9d7df73fd6a5a6474d280db8cb6d6b475fb
lauechev <laura.echeverri3@gmail.com>
2025-03-03 14:07:29 -0500

    🪺 add html and css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles.css';
+import React from 'react';
 
-function App() {
+const images = [];
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="title">
+        <h1>Zesty</h1>
+        <h2>
+          A photography project
+          <br />
+          by Ella Fieldling
+        </h2>
+      </div>
+
+      <div className="img-container">
+        <img src="" alt="" />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
